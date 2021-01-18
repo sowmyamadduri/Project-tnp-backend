063dfdc1dbd5abec5aaf8f678448219381890d91
sowmyamadduri <77627067+sowmyamadduri@users.noreply.github.com>
2021-01-18 20:49:33 +0530

    Add files via upload

---
+++ UpdatePasswordApplication.java
@@ -0,0 +1,13 @@
+package com.tnp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdatePasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdatePasswordApplication.class, args);
+	}
+
+}
